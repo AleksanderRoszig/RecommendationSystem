c1f46fc028caf76140049559352e36e13e9633ee
AleksanderRoszig <aleksrosz@gmail.com>
2018-10-26 01:07:22 +0200

    Calculations are okay

--- src/com/company/MemoryBased.java
+++ src/com/company/MemoryBased.java
@@ -16,13 +16,15 @@ public class MemoryBased {
     private double averageRatingComparedUser;
     private double inputUserRatingSumP = 0;
     private double comparedUserRatingSumK = 0;
-    private int itemI;
+    //private int itemI;
     private double weightedRating;
     private double averageRatingExI;
     public int[][] setOfUsersU = new int[100][100];
     private double[] arrayofNorWpk = new double[100];
     public double[] weighted = new double[1000];
-
+    private int[] itemI = new int[11];
+    public int howmuch12;
+    int u;
     int i;
     int m;
     int id; //idUser from dataBaseOfUsers
@@ -30,6 +32,15 @@ public class MemoryBased {
     int howMuchProductsComparedUserRated;
     int[] productsRatedByBothUsers = new int[11];
 
+
+    private void whichProdutsNotComaredUser(double[][] comparedUser) {
+        for (int i = 1; i < comparedUser.length; i++) {
+            if (comparedUser[i][0] == 0) {
+                howmuch12++;
+                itemI[i] = 1;
+            } else itemI[i] = 0;
+        }
+    }
 
     //check how much products rated by both users
     private void whichProductsAreRatedByBothUsers(double[][] comparedUser, double[][] dataBase, int id) {
@@ -44,8 +55,7 @@ public class MemoryBased {
             } else
                 productsRatedByBothUsers[i] = 1;
         }
-    }//working good
-
+    }
 
     private int howMuchProductsBothUsersRated() {
         howMuchProductsBothUsersRated = 0;
@@ -53,9 +63,7 @@ public class MemoryBased {
             howMuchProductsBothUsersRated = howMuchProductsBothUsersRated + productsRatedByBothUsers[i];
         }
         return howMuchProductsBothUsersRated;
-    }//working good
-
-    //tutaj wychodzi howmuch = 0 cały czas
+    }
 
     //check how much products rated by user ComparedUser
     private int howMuchProductsCompaRated(double[][] comparedUser) {
@@ -68,30 +76,39 @@ public class MemoryBased {
     //average rating of all items rated by user ComparedUser exception item I
     private double averageComparedUsrI(double[][] comparedUser, int itemI) {
         double sumofallratingexi = 0;
+        int howmuch = 0;
         for (i = 1; i < comparedUser.length; i++) {
-            if (i == itemI) i++;
-            sumofallratingexi = sumofallratingexi + comparedUser[i][0];
+            if (i != itemI) {
+                sumofallratingexi = sumofallratingexi + comparedUser[i][0];
+                howmuch++;
+            }
         }
-        return sumofallratingexi;
+        return sumofallratingexi / howmuch;
     }
 
     //average rating of all items rated by user dataBase exception item I
     private double averageDataBaUsrI(double[][] dataBase, int itemI, int id) {
         double sumofallratingexi = 0;
-        for (i = 1; i < dataBase.length; i++) {  //is this lenght ok?
-            if (i == itemI) i++;
-            sumofallratingexi = sumofallratingexi + dataBase[id][i];
+        int howmuch = 0;
+        for (i = 1; i <= dataBase.length; i++) {
+            if (i != itemI) {
+                sumofallratingexi = sumofallratingexi + dataBase[id][i];
+
+                howmuch++;
+            }
         }
-        return sumofallratingexi;
+        return sumofallratingexi / howmuch;
     }
 
     //comparedUser that's our user from User class, DataBase that's for comparing
     public MemoryBased(double[][] comparedUser, double[][] dataBase) {
-        id = 0; //check this for for todo
-        whichProductsAreRatedByBothUsers(comparedUser,dataBase, id);
+        u = 0;
+        howmuch12 = 0;
+        whichProductsAreRatedByBothUsers(comparedUser, dataBase, id);
+        whichProdutsNotComaredUser(comparedUser);
 
         //Calculate Pearson correlation coefficient
-        for(int id = 0; id < 1; id++){
+        for (int id = 0; id < dataBase.length; id++) {
             counter2 = 0;
             denominator2 = 0;
             weightedRating = 0;
@@ -99,29 +116,27 @@ public class MemoryBased {
             inputUserRatingSumP = 0;
             comparedUserRatingSumK = 0;
 
-        for (i = 1; i < comparedUser.length; i++) {
+            for (i = 1; i < comparedUser.length; i++) {
                 inputUserRatingSumP = inputUserRatingSumP + comparedUser[i][0];
                 comparedUserRatingSumK = comparedUserRatingSumK + dataBase[id][i];
-        }
-        averageRatingInputUser = (inputUserRatingSumP / comparedUser.length);
-        averageRatingComparedUser = (comparedUserRatingSumK / comparedUser.length);
-
-        //------------------------sprawdzić w dół
-
-        //sumA = left side of counter, sumB = right side of counter
-        for (i = 1; i < comparedUser.length; i++) {
-            if (productsRatedByBothUsers[i] == 1) {
-                sumA = sumA + (comparedUser[i][0] - averageRatingInputUser);
-                sumB = sumB + (dataBase[id][i] - averageRatingComparedUser);
             }
-        }//end for(int i)
-        counter1 = sumA * sumB;
-        for (i = 1; i < comparedUser.length; i++) {
-            if (productsRatedByBothUsers[i] == 1) {
-                sumDA = sumDA + Math.sqrt(Math.pow(comparedUser[i][0] - averageRatingInputUser, 2));
-                sumDB = sumDB + Math.sqrt(Math.pow(dataBase[id][i] - averageRatingComparedUser, 2));
-            }
-        }//end for(int i)
+            averageRatingInputUser = (inputUserRatingSumP / comparedUser.length);
+            averageRatingComparedUser = (comparedUserRatingSumK / comparedUser.length);
+
+            //sumA = left side of counter, sumB = right side of counter
+            for (i = 1; i < comparedUser.length; i++) {
+                if (productsRatedByBothUsers[i] == 1) {
+                    sumA = sumA + (comparedUser[i][0] - averageRatingInputUser);
+                    sumB = sumB + (dataBase[id][i] - averageRatingComparedUser);
+                }
+            }//end for(int i)
+            counter1 = sumA * sumB;
+            for (i = 1; i < comparedUser.length; i++) {
+                if (productsRatedByBothUsers[i] == 1) {
+                    sumDA = sumDA + Math.sqrt(Math.pow(comparedUser[i][0] - averageRatingInputUser, 2));
+                    sumDB = sumDB + Math.sqrt(Math.pow(dataBase[id][i] - averageRatingComparedUser, 2));
+                }
+            }//end for(int i)
             denominator1 = sumDA * sumDB;
             Wpk = counter1 / denominator1;
             //end Calculate Pearson correlation coefficient
@@ -129,20 +144,31 @@ public class MemoryBased {
             //emphasizing the difference in Pearson coefficient between User P and oth er users
             int q = 2; //this parameter specifies how much should be emphasized difference
             norWPK = Wpk * Math.pow(Math.abs(Wpk), q - 1);
-            arrayofNorWpk[id] = norWPK; //check this id!!todo
+            arrayofNorWpk[id] = norWPK; //its okay
         }//end for(id)
+        //end emphasing
+        u = 1;
+        do { //do for item
+            id = 0;
+            if (itemI[u] == 1) {//czy item nalezy do zboiru nieratowanych przez comparedusera
 
-            //end emphasing
-            //-------------------------------------------^ good
+                do {// for ids
+                    //weighted rating of other users of the I item counted for user ComparedUser
+                    //--------------------------------------
+                    counter2 = counter2 + (dataBase[id][u] - averageDataBaUsrI(dataBase, u, id)) * arrayofNorWpk[id];
+                    denominator2 = denominator2 + Math.abs(norWPK);
+                    id++;
+                } while (id < dataBase.length);
 
-            //weighted rating of other users of the I item counted for user ComparedUser
-            //--------------------------------------
-            for (int k = 0; k < dataBase.length; k++) {
-                counter2 = counter2 + (dataBase[id][itemI] - averageDataBaUsrI(dataBase, itemI, id)) * arrayofNorWpk[id];
-                denominator2 = denominator2 + Math.abs(norWPK);
+                weighted[u] = averageComparedUsrI(comparedUser, u) + counter2 / denominator2;
+
             }
-                weighted[itemI] = averageComparedUsrI(comparedUser, itemI) + counter2 / denominator2;
-                System.out.println(weighted[itemI]);
-
+            u++;
+        } while (u < 10); //end while(itemI[i] != 0)
+        for (u = 0; u < itemI.length; u++) {
+            if (itemI[u] == 1) System.out.println(weighted[u] + " weighted for " + u);
         }
+
+
     }
+}
